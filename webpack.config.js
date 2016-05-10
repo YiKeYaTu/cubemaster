ef1f89a6fef0ba8443fb2aafa6b414ed7f3987de
YiKeYaTu <liangchen005005@163.com>
2016-05-10 15:31:29 +0800

    增加配置文件

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var path = require("path");
+var webpack = require('webpack');
+module.exports = {
+    entry: {
+        index: ["./src/entry/index.jsx"],
+        data: ["./src/entry/data.jsx"],
+        combination: ["./src/entry/combination.jsx"],
+        vendor: ['react', 'react-dom']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/, 
+                loader: "style!css!less" 
+            },
+            {
+                test: /\.jsx?$/, 
+                loader: "babel", 
+                exclude: /node_modules/, 
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url',
+                query: {
+                    limit: 10000
+                }
+            }
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, "app"),
+        filename: "[name].min.js"
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin('vendor', 'react.min.js'),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': '"production"'
+        }),
+    ]
+};
