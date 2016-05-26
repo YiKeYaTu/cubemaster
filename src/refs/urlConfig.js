75ff339fab1221974191db7c2e88e636cb1a7138
YiKeYaTu <liangchen005005@163.com>
2016-05-26 19:27:14 +0800

    fix

--- src/refs/urlConfig.js
+++ src/refs/urlConfig.js
@@ -4,7 +4,7 @@ let customConf = {
     loginServer: 'http://172.22.147.2/cubemaster/servlet/LoginCheckServlet',
     loginOutServer: 'http://172.22.147.2/cubemaster/servlet/LogoutServlet',
     runServer: 'http://172.22.147.2/cubemaster/servlet/RunServlet',
-    datasetServer: 'http://172.22.147.2/cubemaster/servlet/DataSetListServlet',
+    datasetServer: 'http://172.22.147.2/cubemaster/servlet/DataSetListServlet'
 }
 
 export default customConf
