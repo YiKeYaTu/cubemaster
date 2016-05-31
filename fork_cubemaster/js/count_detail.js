e08e75a4eadafbdbbc790b01d7d638e6c2982bee
liuyanhao <374659635@qq.com>
2016-05-31 20:35:13 +0800

    更新了算法页面的交互

--- fork_cubemaster/js/count_detail.js
+++ fork_cubemaster/js/count_detail.js
@@ -4,6 +4,92 @@
 
 window.onload=function() {
 
+	//获取算法基本数据
+	function loadBasicData() {
+		var xmlhttp = new XMLHttpRequest();
+		var url = "http://172.22.146.5/FileSystem/servlet/AlgorithmDetailsServlet?protocol=A-2-3-request&algorithm_id=9";
+		// var url = "http://172.22.147.5:8080/FileSystem/servlet/AlgorithmMenuServlet?algorithm_id=8";
+		xmlhttp.onreadystatechange = function () {
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				console.log(xmlhttp.responseText)
+				var content = JSON.parse(xmlhttp.responseText)
+				addBasicData(content);
+			}
+		}
+		xmlhttp.open("GET", url, true);
+		xmlhttp.send();
+	}
+	loadBasicData()
+
+	//将基本信息添加到节点上
+	function addBasicData(content) {
+		var brief = content.algorithm.description;
+		//算法名称
+		var name = content.algorithm.algorithm_name;
+		//算法类型
+		var tasks = content.algorithm.associated_tasks;
+		//作者
+		var author = content.user_name;
+		//测试数据集
+		var dataTest = content.algorithm.data_test;
+		//简介
+		var brief = content.algorithm.description;
+		//参数个数
+		var paraCount = content.algorithm.parameter_count;
+		//平台
+		var platform = content.algorithm.platform;
+		//最新提交时间
+		var submitTime = content.algorithm.submit_datetime;
+		//关键字
+		var keyword = content.key_word[0].keyword;
+		//数据需求
+		// var requirements = content.algorithm.associated_requirements;
+		//输入格式
+		var inPattern = content.algorithm.in_pattern;
+		//输出格式
+		var outPattern = content.algorithm.out_pattern;
+
+		document.querySelector(".count-name").innerHTML = name;
+		//判断匿名
+		if(author) {
+			document.querySelector(".author").innerHTML = author;
+		}
+		document.querySelector(".update-time").innerHTML = submitTime;
+		document.querySelector(".brief-content").innerHTML = brief
+		document.querySelector(".pro-type span").innerHTML = tasks;
+		document.querySelector(".pro-keyword span").innerHTML = keyword;
+		document.querySelector(".pro-platform span").innerHTML = platform;
+		document.querySelector(".output-style-content").innerHTML = outPattern;
+		document.querySelector(".input-style-content").innerHTML = inPattern;
+		document.querySelector(".test-data-content").innerHTML = dataTest;
+
+		//参数列表
+		var paraList = content.parameters;
+		paraList.forEach(function (item,index,array) {
+			var paraObj = document.createElement('ul');
+
+			//参数名称
+			var paraName = document.createElement("li");
+			var nodeName = document.createTextNode(item.parameter_name);
+			paraName.appendChild(nodeName);
+
+			//参数类型
+			var paraType = document.createElement("li");
+			var nodeType = document.createTextNode(item.parameter_type);
+			paraType.appendChild(nodeType);
+
+			//参数值
+			var paraVal = document.createElement("li");
+			var nodeVal = document.createTextNode(item.parameter_value);
+			paraVal.appendChild(nodeVal);
+
+			paraObj.appendChild(paraName);
+			paraObj.appendChild(paraType);
+			paraObj.appendChild(paraVal);
+			document.querySelector(".para-list-content").appendChild(paraObj);
+		})
+	}
+
 	//模态框状态
 	var modalStatus = document.querySelector("#modal-overlay");
 	//列表栏状态
